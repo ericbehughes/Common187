80e493355adb45d9e4470d17f8e157c4306246ec
Jepaz <jepaz@gmail.com>
2016-10-27 10:30:02 -0400

    Commented out code

--- src/group187/util/ListUtilities.java
+++ src/group187/util/ListUtilities.java
@@ -15,6 +15,9 @@ public class ListUtilities {
 	public static void saveListToTextFile(File file) throws IOException{
 		if (file == null)
 			throw new FileNotFoundException("The file cannot be found");
+		
+		
+		/**
 		try {
 			if (file.getAbsolutePath().indexOf("customer") != -1){
 				String outFilePath = file.getAbsolutePath()
@@ -36,7 +39,7 @@ public class ListUtilities {
 			catch (FileNotFoundException fe){
 				System.out.println(fe.getMessage());
 			}
-		
+		*/
 			//String outFilePath = file.getPath().substring(0, file.getPath().lastIndexOf("\\")) + "\\SortedReservationList.txt";
 	}
 }
